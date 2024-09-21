6011fbfceeb58ce7a4e4f5c5c3c70640d8102967
Charlie <charlieboyle252@gmail.com>
2024-09-21 12:32:20 -0500

    timers test project

---
+++ index.js
@@ -0,0 +1,10 @@
+const orange = document.getElementById('orange')
+const button = document.getElementById('button-one')
+
+
+function appear() {
+
+    setTimeout(() => {
+        orange.style.display = 'block';
+    }, 2000); 
+}
